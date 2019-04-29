520f9589a67b2f6b28debde2de9f39365a3473d5
Benjamin <maurouardbenjamin@gmail.com>
2019-04-29 14:40:56 +0200

    modif du index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>test 2</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In perspiciatis alias praesentium deserunt, eligendi impedit ab dolore illo rerum, vitae nostrum dignissimos autem reprehenderit natus porro reiciendis? Magnam, veniam saepe.</p>
+</body>
+</html>
