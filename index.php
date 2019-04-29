71c64f2ec551c39c2ccd09a3ec7d379ff10f0525
Benjamin <maurouardbenjamin@gmail.com>
2019-04-29 14:45:26 +0200

    correction lien css

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" media="screen" href="asset/styles.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="styles.css">
     <title>test 2</title>
 </head>
 <body>
