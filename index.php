87fe457151dcc26b090118ba94631c1bddafa5e0
Maxime <maxime.coolombier@gmail.com>
2019-04-29 14:58:13 +0200

    ajout d'une navigation

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" media="screen" href="asset/styles.css">
-    <script src="asset/main.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/styles.css">
+    <script src="assets/main.js"></script>
     <title>test 2</title>
 </head>
 <body>
